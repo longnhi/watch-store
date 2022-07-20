0d88d113c7bc9dd0147669b80feb64f6f900a452
longnhi <nhilong0711@gmail.com>
2022-07-20 16:46:38 +0700

    server connect db sql

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 const cors = require("cors");
+
+const db =  require('./config/db');
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req,res) => {
     res.json("Hello world!");
-})
+});
 
 app.listen(3001, () => {
     console.log("Server running on port 3001");
